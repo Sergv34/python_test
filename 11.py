db491b62627fb392c4ea6caa7388b711a8992747
Sergv34 <wwerw0900@gmail.com>
2023-01-15 18:33:54 +0300

    four commit test def 3

--- 11.py
+++ 11.py
@@ -1,15 +1,7 @@
 def is_len():
-    s = input().split()
-    if len(s) < 6:
-        return False
-    else:
-        return True
 
-
-a = [x for x in input().split()]
-lst = [x for x in a if len(x) >= 6]
 lst1 = 5
 lst2 = 6
-print(*lst)
+
 print(lst1)
 print(lst2)
